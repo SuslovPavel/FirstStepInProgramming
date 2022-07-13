bd120fb6fb26f0241fc61d6b281125cde2e5b47f
SuslovPavel <pavel7511084@gmail.com>
2022-07-14 00:10:47 +0500

    Add Code SolutionTask3

--- SolutionTask3/Program.cs
+++ SolutionTask3/Program.cs
@@ -4,16 +4,16 @@ if(inputLine != null)
 {
     int inputNumber = int.Parse(inputLine);
 
-    int startNumber = (inputNumber* -1);
+    int startNumber = 1;
 
-    string lineOutput = "";
+    string lineOutput = "0";
 
-    while(startNumber < inputNumber)
+    while(startNumber <= inputNumber)
     {
-        lineOutput = lineOutput+startNumber+", ";
+        lineOutput = startNumber*(-1)+", " +lineOutput+", "+startNumber;
         startNumber++;
     }
-        lineOutput = lineOutput+inputNumber;
+        // lineOutput = lineOutput+inputNumber;
     
 
     Console.WriteLine(lineOutput);
