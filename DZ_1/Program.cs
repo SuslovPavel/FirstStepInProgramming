8fea7afb16d45b358ca9609a084f472d59947b08
SuslovPavel <pavel7511084@gmail.com>
2022-07-15 17:21:07 +0500

    Add DZ_1

---
+++ DZ_1/Program.cs
@@ -0,0 +1,40 @@
+﻿string? inputLineOne = Console.ReadLine();
+string? inputLineTwo = Console.ReadLine();
+string? inputLineThree = Console.ReadLine();
+
+if(inputLineOne != null && inputLineTwo != null && inputLineThree != null)
+{
+    int inputNumberOne =int.Parse(inputLineOne);
+    int inputNumberTwo =int.Parse(inputLineTwo);
+    int inputNumberThree =int.Parse(inputLineThree);
+
+    if(inputNumberOne > inputNumberTwo)
+    {
+      if(inputNumberTwo > inputNumberThree)
+      {   
+        Console.WriteLine (inputLineOne);
+      }
+      if(inputNumberOne > inputNumberThree)
+      {
+       Console.WriteLine (inputLineOne); 
+      }
+      if(inputNumberOne < inputNumberThree)
+      {
+        Console.WriteLine (inputLineThree);
+      }         
+    }
+    if(inputNumberOne < inputNumberTwo)
+    {
+      if(inputNumberTwo < inputNumberThree)
+      {   
+        Console.WriteLine (inputLineThree);
+      }
+      if(inputNumberTwo > inputNumberThree)
+      {
+        Console.WriteLine (inputLineTwo);
+
+      }          
+    }
+
+}   
+      
