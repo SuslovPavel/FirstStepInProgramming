809cd9dd1d1c7f6a1f60d78046e27dbc67db3388
SuslovPavel <pavel7511084@gmail.com>
2022-07-15 11:29:19 +0500

    Add DZ_0

---
+++ DZ_0/Program.cs
@@ -0,0 +1,18 @@
+﻿string? inputLineOne = Console.ReadLine();
+string? inputLineTwo = Console.ReadLine();
+
+if(inputLineOne != null && inputLineTwo != null)
+{
+    int inputNumberOne =int.Parse(inputLineOne);
+    int inputNumberTwo =int.Parse(inputLineTwo);
+
+    if(inputNumberTwo > inputNumberOne)
+    {   
+        Console.WriteLine (inputLineTwo, ">" ,inputLineOne);
+    }    
+    else
+    {
+       Console.WriteLine (inputLineOne, ">" ,inputLineTwo); 
+    }
+}
+// Андрей подскажите как исправить код чтобы при вводе 5,7 ответ в консоле выводился в виде "7 > 5", а не просто 7 как сейчас ????????
