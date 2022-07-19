5d3017ca186e00ba6a6c3648c6a2484d4adf2354
SuslovPavel <pavel7511084@gmail.com>
2022-07-20 00:19:13 +0500

    add task16

---
+++ SolutionTask16/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+
+
+Console.WriteLine("Введите первое число");
+string? inputLineOne = Console.ReadLine(); 
+Console.WriteLine("Введите второе число");
+string? inputLineTwo = Console.ReadLine();
+
+
+if(inputLineOne != null && inputLineTwo != null)
+{
+    int NumberOne =int.Parse(inputLineOne);
+    int NumberTwo =int.Parse(inputLineTwo);
+    int NumberTree = NumberTwo / NumberOne;
+    int NumberFour = NumberOne / NumberTwo;
+
+    if((NumberTree == NumberOne) || (NumberFour == NumberTwo))
+    {   
+        Console.WriteLine("yes");
+    }    
+    else
+    {
+       Console.WriteLine("No"); 
+    }
+
+}
