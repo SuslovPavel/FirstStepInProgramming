14439d9cb7c4a904221186fe6fa0765f811faf8e
SuslovPavel <pavel7511084@gmail.com>
2022-07-19 23:29:39 +0500

    Add Task14

---
+++ SolutionTask14/Program.cs
@@ -0,0 +1,17 @@
+﻿string? inputLineOne = Console.ReadLine(); 
+
+if(inputLineOne !=null)
+{
+    int NumberOne =int.Parse(inputLineOne);
+    int result1 = NumberOne % 7;
+    int result2 = NumberOne % 23;
+
+    if(result1 == 0 && result2 == 0)
+    {   
+        Console.WriteLine("Yes");
+    }
+    else
+    {
+        Console.WriteLine("No");
+    }
+}
