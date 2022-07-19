662db27eff9cdb1d0aa7c1a3427b6794aae87230
SuslovPavel <pavel7511084@gmail.com>
2022-07-19 23:05:26 +0500

    Add Task 12

---
+++ SolutionTask12/Program.cs
@@ -0,0 +1,19 @@
+﻿string? inputLineOne = Console.ReadLine(); 
+string? inputLineTwo = Console.ReadLine();
+
+if(inputLineOne != null && inputLineTwo != null)
+{
+    int NumberOne =int.Parse(inputLineOne);
+    int NumberTwo =int.Parse(inputLineTwo);
+    int NumberTree = NumberTwo % NumberOne;
+
+    if(NumberTree == 0)
+    {   
+            Console.WriteLine("yes");
+    }    
+    else
+    {
+       Console.WriteLine(NumberTree); 
+    }
+
+}
