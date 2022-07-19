c5dbf510dff5a8da2b440a4bc4a3f5dec39b83cf
SuslovPavel <pavel7511084@gmail.com>
2022-07-19 15:08:24 +0500

    Add SolutionTask9 three code options

---
+++ SolutionTask9/Program.cs
@@ -0,0 +1,43 @@
+﻿System.Random numberSintezator = new Random ();
+int number = numberSintezator.Next(10,100);
+Console.WriteLine(number);
+// Вариант 1
+int firstNumber = number/10;
+int secondNumber = number%10;
+
+if (firstNumber>secondNumber)
+{
+    Console.WriteLine(firstNumber);
+}
+else
+{
+    Console.WriteLine(secondNumber);
+}
+
+// Вариант 2
+
+int NumberK =new Random().Next(10,100);
+Console.Write ("The Random number is: ");
+Console.WriteLine (NumberK);
+
+string stringNumber = NumberK.ToString();
+Console.Write ("the Biggest digit is: ");
+if(stringNumber[0]>stringNumber[1])
+{
+    Console.WriteLine(stringNumber[0]);
+}
+else
+{
+    Console.WriteLine(stringNumber[1]);
+}
+
+// Вариант 3
+
+char[] digits = numberSintezator.Next(10,100).ToString().ToCharArray();
+Console.WriteLine(digits);
+
+firstNumber = ((int)digits[0])-48;
+secondNumber = ((int)digits[1])-48;
+
+int resulteNember = firstNumber>secondNumber?resulteNember=firstNumber:resulteNember=secondNumber;
+Console.WriteLine(resulteNember);
