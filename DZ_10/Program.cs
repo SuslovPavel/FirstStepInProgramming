6e0075ca850386f361e871100c317e13854b5cde
SuslovPavel <pavel7511084@gmail.com>
2022-07-20 16:39:51 +0500

    Add DZ_10 Variant2

--- DZ_10/Program.cs
+++ DZ_10/Program.cs
@@ -19,12 +19,30 @@ void Variant1()
     }
 }
 
-
 //Вариант 2
 
+void variant2()
+{
+    Console.Clear();
+    Console.Write("Введите трёхзначное число: "); // принимаем значение с консоли
+    string? inputLineOne = Console. ReadLine();
+
+    int NumberOne =int.Parse(inputLineOne);      // создание переменной
+    int NumberTwo =(NumberOne%100)/10;           // вычисление второй цифры из введёного числа
+    if(NumberOne < 100 || NumberOne > 999)       // проверка введёного числа
+    {
+        Console.Write("Ошибка! Введено неверное число");
+    }
+    else
+    {
+        Console.Write(NumberTwo);
+    }
+
+}
 
 
+// Variant1();
 
+variant2();
 
-
-Variant1();
+// Одновременно оба варианта не ратотают, один приходится отключать. Подскажите пожалуйста как это исправить
