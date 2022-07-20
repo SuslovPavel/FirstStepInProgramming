4a4fe9ec8b0fcd6aed7d51a1c3ce89fadba388ec
SuslovPavel <pavel7511084@gmail.com>
2022-07-20 17:43:08 +0500

    Add DZ_15

---
+++ DZ_15/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+Console.Write("Введите цифру соответствующую дню недели: ");   // принимаем значение с консоли
+string? inputLineOne = Console.ReadLine(); 
+
+int numberDay =int.Parse(inputLineOne);
+if(numberDay == 7 || numberDay == 6 )
+{
+    Console.Write("Этот день является выходным");
+}
+else
+{
+   Console.Write("Этот день НЕ является выходным"); 
+}
