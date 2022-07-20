d98f28735cc88e365a180f4f8247167868cf4bc4
SuslovPavel <pavel7511084@gmail.com>
2022-07-20 16:57:01 +0500

    Add DZ_13

---
+++ DZ_13/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+
+Console.Write("Введите трёхзначное число: "); // принимаем значение с консоли
+string? inputLineOne = Console.ReadLine();
+
+char[] M = inputLineOne.ToCharArray();       // преобразуем значение в массив
+if (M.Length < 3)                            // выставляем условие что длина массива не менее 3х элементов
+{
+    Console.Write("Третьей цифры нет");
+}
+else
+{
+    Console.Write("Вторая цифра числа: ");
+    Console.WriteLine(M[2]);
+}
