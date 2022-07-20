58312a1366b07b7f44c3d86f2852de1aa03046c2
SuslovPavel <pavel7511084@gmail.com>
2022-07-20 14:43:55 +0500

    Add DZ_10 Variant1

---
+++ DZ_10/Program.cs
@@ -0,0 +1,30 @@
+﻿// Вариант 1
+
+void Variant1()
+{
+    Console.Clear();
+
+    Console.Write("Введите трёхзначное число: "); // принимаем значение с консоли
+    string? inputLineOne = Console.ReadLine();
+
+    char[]M = inputLineOne.ToCharArray();        // преобразуем значение в массив
+    if( M.Length != 3)                           // выставляем условие что длина массива не более 3х элементов
+    {
+        Console.Write("Ошибка! Введено неверное число");
+    }
+    else
+    {
+        Console.Write("Вторая цифра числа: ");
+        Console.WriteLine(M[1]);
+    }
+}
+
+
+//Вариант 2
+
+
+
+
+
+
+Variant1();
