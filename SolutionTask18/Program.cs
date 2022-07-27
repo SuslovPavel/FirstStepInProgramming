20a69ad53f0ae9bdee2e94c145cc0203f92149ec
SuslovPavel <pavel7511084@gmail.com>
2022-07-27 23:42:03 +0500

    Add SolutionTask18

---
+++ SolutionTask18/Program.cs
@@ -0,0 +1,23 @@
+﻿// Метод принимает значение от 1-4 и выдает допустимый диапазон
+
+
+void printAnswer(int number)
+{
+    if(number==1) Console.WriteLine("Допустимо: x>0;y>0");  // проверяет значение и выводит результат на консоль
+
+    if(number==2) Console.WriteLine("Допустимо: x<0;y>0");
+    
+    if(number==3) Console.WriteLine("Допустимо: x<0;y<0");
+
+    if(number==4) Console.WriteLine("Допустимо: x>0;y<0");
+
+}
+string? inputLine = Console.ReadLine();            // считывает значения с консоли
+
+if(inputLine != null)                              // проверка условия
+{
+    int inputNumber = int.Parse(inputLine);       // распознаёт значение и переводит его в строчку  
+    
+    printAnswer(inputNumber);
+   
+}
