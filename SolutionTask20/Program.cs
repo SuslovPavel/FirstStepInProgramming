b7e97f1b08d0f72b71d39be59f3b42c39c6d22e8
SuslovPavel <pavel7511084@gmail.com>
2022-07-28 21:37:49 +0500

    Add SolutionTask20

---
+++ SolutionTask20/Program.cs
@@ -0,0 +1,33 @@
+﻿int coordXPointA;  // глобальная переменная, необходима т.к. переменные внутри метода не видны (тип данных внутри метода уже не указывается)
+int coordYPointA;
+int coordXPointB;
+int coordYPointB;
+double LengthAB;
+
+// Считывает координаты точек А и В
+void readDataOfPoint()
+{
+    Console.WriteLine("Введите коордигату Х точки А");         // Выводит сообщение на консоль
+    coordXPointA = int.Parse(Console.ReadLine());          // Распознаёт введёное значение
+
+    Console.WriteLine("Введите коордигату Y точки А");
+    coordYPointA = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Введите коордигату Х точки B");
+    coordXPointB = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Введите коордигату Y точки B");
+    coordYPointB = int.Parse(Console.ReadLine());
+}
+
+// Вычисляет расстояние между точками А и В
+void conculateLengthAB()
+{
+    // мат. действие извлечение корня                 // мат. действие возведение в степень
+LengthAB = Math.Sqrt(Math.Pow((coordXPointA - coordXPointB), 2) + Math.Pow((coordYPointA - coordYPointB), 2));  // тип double хранит значения с запятыми
+}
+
+readDataOfPoint();      // Активация метода
+conculateLengthAB();
+
+Console.WriteLine(LengthAB);
