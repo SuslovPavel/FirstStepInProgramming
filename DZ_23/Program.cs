33c36793d984b7734b0117d1e666063a23cd6e84
SuslovPavel <pavel7511084@gmail.com>
2022-08-03 00:24:28 +0500

    Add DZ23

---
+++ DZ_23/Program.cs
@@ -0,0 +1,31 @@
+﻿int lineN;
+int lineNN;
+
+Console.Write("Введите число: ");
+string? inputLineN = Console.ReadLine();     // принимает значения с консоли
+
+void Method1()
+{
+    if (inputLineN != null)                      // борьба с пустыми ссылками, нужно чтоб программа не ругалась
+    {  
+        int numberN = int.Parse(inputLineN);    // распознает значения принятые с консоли
+
+        string lineN = "";
+        string lineNN = string.Empty;          // string.Empty; = пустая строка = "";
+        int s = 1;
+        while (s <= numberN)
+        {
+            lineN = lineN + s + " ";                  // выводит массив по заданому числу
+            lineNN = lineNN + (s * s * s) + " ";      // выводит массив куба по заданному числу
+            s++;                                      // увеличивает s на 1
+        }
+        Console.WriteLine(lineN);
+        Console.WriteLine(lineNN);
+    }
+   
+}
+    
+Method1();
+
+
+
