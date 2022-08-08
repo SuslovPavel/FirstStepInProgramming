23c6d11609c6b330e2128b3933ac2e16d1043b3d
SuslovPavel <pavel7511084@gmail.com>
2022-08-09 01:48:59 +0500

    Add SolutionTask30

---
+++ SolutionTask30/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+//Например: [1,0,1,1,0,1,0,0]
+
+System.Random numberSintezator = new Random();  // Создаём: Рандомайзер, глобальную переменную, выделяем под него память (new)
+
+void VariantNaive()
+{
+    int i = 0;
+    Console.Write("[");                                // выводит скобку
+    while(i<7)
+    {
+        Console.Write(numberSintezator.Next(0,2)+","); // выводит случайное число от 0 до 1
+        i++;       
+    }
+    Console.Write(numberSintezator.Next(0,2));        // выводит последнее случайное число от 0 до 1
+    Console.Write("]");                               // выводит скобку
+
+}
+VariantNaive();
