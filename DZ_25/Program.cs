5128d1e4045589b1f6da074ffd7cd47fd67b30bc
SuslovPavel <pavel7511084@gmail.com>
2022-08-09 23:54:26 +0500

    add dz_25

---
+++ DZ_25/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+
+void Read()
+{
+    Console.Write("Введите число: ");                               // Принимает значение с консоли, уберает null, расрлзнает значение
+    string inputLineNumberA = Console.ReadLine() ?? "";
+    int inputNumberA = int.Parse(inputLineNumberA);
+
+    Console.Write("В какую степень возвести число: " + (inputNumberA) + "? ");
+    string inputLineNumberB = Console.ReadLine() ?? "";
+    int inputNumberB = int.Parse(inputLineNumberB);
+}
+
+void Method1()
+{
+    int i = 1;
+    int numC = 0;
+    while (i < inputNumberB)                           // Повторяет цикл количество раз inputNumberB
+    {
+        numC = (numC * inputNumberA);
+        i++;
+    }
+    Console.WriteLine(numC);
+}
+
+Read();
+Method1();
+
+
+
+
