8263bb90c66751ce2fce163b35c87002e0656d54
SuslovPavel <pavel7511084@gmail.com>
2022-08-13 20:06:22 +0500

    add dz_27

---
+++ DZ_27/Program.cs
@@ -0,0 +1,27 @@
+﻿
+// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе. //
+
+
+Console.Write("Введите число: ");
+string inputLineNumberA = Console.ReadLine() ?? "";
+int numberA = int.Parse(inputLineNumberA);
+int numberB = 0; 
+
+// void MethodInt()
+// {
+//     while (numberA > 0)
+//     {
+//         numberB = numberB + numberA % 10;
+//         numberA = numberA / 10;
+//     }
+//     Console.WriteLine(numberB);
+// }
+// MethodInt();
+
+char[] M = inputLineNumberA.ToCharArray();
+int i= M.Length;
+while (M[i]<= i)
+{
+numberB = M[0]+M[i];
+}
+Console.WriteLine(numberB);
