1025ad6d6e1206ec6327381504b3903a94c4f459
SuslovPavel <pavel7511084@gmail.com>
2022-08-13 19:57:56 +0500

    Added Correct

--- DZ_10/Program.cs
+++ DZ_10/Program.cs
@@ -5,7 +5,7 @@ void Variant1()
     Console.Clear();
 
     Console.Write("Введите трёхзначное число: "); // принимаем значение с консоли
-    string? inputLineOne = Console.ReadLine();
+    string? inputLineOne = Console.ReadLine()?? "";
 
     char[]M = inputLineOne.ToCharArray();        // преобразуем значение в массив
     if( M.Length != 3)                           // выставляем условие что длина массива не более 3х элементов
