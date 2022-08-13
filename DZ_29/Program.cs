a47f5a1f35c05742f32b9d3f4c4bbe9a4ef1dd2c
SuslovPavel <pavel7511084@gmail.com>
2022-08-14 00:43:14 +0500

    add dz_29

---
+++ DZ_29/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+
+
+System.Random numberSintezator = new Random();  // Создаём: Рандомайзер, глобальную переменную, выделяем под него память (new)
+Console.Write("Ведите число равное длине массива: ");
+string inputNumber = Console.ReadLine()?? "";
+int lengthArrey = int.Parse(inputNumber);
+int i = 0;
+
+
+void VariantNaive()
+{
+    //int i = 0;
+    Console.Write("[");                                // выводит скобку
+    while(i<(lengthArrey-1))
+    {
+        Console.Write(numberSintezator.Next(0,11)+","); // выводит случайное число от 0 до 10
+        i++;       
+    }
+    Console.Write(numberSintezator.Next(0,11));        // выводит последнее случайное число от 0 до 10
+    Console.Write("]");                               // выводит скобку
+
+}
+VariantNaive();
