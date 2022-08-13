7f14934549d1d2fa193f282b8a8021755909c8b2
SuslovPavel <pavel7511084@gmail.com>
2022-08-13 12:18:24 +0500

    Add DZ_25

--- DZ_25/Program.cs
+++ DZ_25/Program.cs
@@ -1,32 +1,30 @@
-﻿
+﻿//   Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B
+
+Console.Write("Введите число: ");                               // Принимает значение с консоли, уберает null, расрлзнает значение
+string inputLineNumberA = Console.ReadLine() ?? "";
+int inputNumberA = int.Parse(inputLineNumberA);
+
+Console.Write("В какую степень возвести число: " + (inputNumberA) + "? ");
+string inputLineNumberB = Console.ReadLine() ?? "";
+int inputNumberB = int.Parse(inputLineNumberB);
 
 
-void Read()
+
+void Calculate()
 {
-    Console.Write("Введите число: ");                               // Принимает значение с консоли, уберает null, расрлзнает значение
-    string inputLineNumberA = Console.ReadLine() ?? "";
-    int inputNumberA = int.Parse(inputLineNumberA);
 
-    Console.Write("В какую степень возвести число: " + (inputNumberA) + "? ");
-    string inputLineNumberB = Console.ReadLine() ?? "";
-    int inputNumberB = int.Parse(inputLineNumberB);
-}
-
-void Method1()
-{
-    int i = 1;
-    int numC = 0;
+    int i = 0;
+    int numC = 1;
     while (i < inputNumberB)                           // Повторяет цикл количество раз inputNumberB
     {
-        numC = (numC * inputNumberA);
+        numC = (numC * inputNumberA);                  // Перемножает введёные данные
         i++;
     }
     Console.WriteLine(numC);
 }
 
-Read();
-Method1();
+Calculate();
 
-
-
+  //   АНДРЕЙ подскажите пожалуйста как обернуть в метод верхний блок кода, необходимый для ввода данных, я как не пытался ерунда выходит  //
+                                //  Заранее благодарю  //
 
