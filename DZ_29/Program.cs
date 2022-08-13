e6fc17a46a86f63d90190116c8e8261dbce11289
SuslovPavel <pavel7511084@gmail.com>
2022-08-14 00:51:18 +0500

    added coment

--- DZ_29/Program.cs
+++ DZ_29/Program.cs
@@ -5,7 +5,7 @@
 System.Random numberSintezator = new Random();  // Создаём: Рандомайзер, глобальную переменную, выделяем под него память (new)
 Console.Write("Ведите число равное длине массива: ");
 string inputNumber = Console.ReadLine()?? "";
-int lengthArrey = int.Parse(inputNumber);
+int lengthArrey = int.Parse(inputNumber);       // принимает число с консоли, распознает его, которое в последствии конвертирует в длину массива
 int i = 0;
 
 
@@ -13,7 +13,7 @@ void VariantNaive()
 {
     //int i = 0;
     Console.Write("[");                                // выводит скобку
-    while(i<(lengthArrey-1))
+    while(i<(lengthArrey-1))                           // задает длину массива
     {
         Console.Write(numberSintezator.Next(0,11)+","); // выводит случайное число от 0 до 10
         i++;       
