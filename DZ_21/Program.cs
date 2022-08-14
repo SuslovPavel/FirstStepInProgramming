ab061f80d5feb7bcf0aa03bff95a61bb706a74ba
SuslovPavel <pavel7511084@gmail.com>
2022-08-14 11:49:27 +0500

    delite null and correct message console

--- DZ_21/Program.cs
+++ DZ_21/Program.cs
@@ -1,4 +1,6 @@
-﻿int coordXPointA;  // глобальная переменная, необходима т.к. переменные внутри метода не видны (тип данных внутри метода уже не указывается)
+﻿//  Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+int coordXPointA;  // глобальная переменная, необходима т.к. переменные внутри метода не видны (тип данных внутри метода уже не указывается)
 int coordYPointA;
 int coordZPointA;
 int coordXPointB;
@@ -9,23 +11,23 @@ double LengthAB;
 // Считывает координаты точек А и В
 void readDataOfPoint()
 {
-    Console.WriteLine("Введите коордигату Х точки А");         // Выводит сообщение на консоль
-    coordXPointA = int.Parse(Console.ReadLine());              // Распознаёт введёное значение
+    Console.WriteLine("Введите координату Х точки А");              // Выводит сообщение на консоль
+    coordXPointA = int.Parse(Console.ReadLine()?? "");              // Распознаёт введёное значение
 
-    Console.WriteLine("Введите коордигату Y точки А");
-    coordYPointA = int.Parse(Console.ReadLine());
+    Console.WriteLine("Введите координату Y точки А");
+    coordYPointA = int.Parse(Console.ReadLine()?? "");
 
-    Console.WriteLine("Введите коордигату Z точки А");
-    coordZPointA = int.Parse(Console.ReadLine());
+    Console.WriteLine("Введите координату Z точки А");
+    coordZPointA = int.Parse(Console.ReadLine()?? "");
 
-    Console.WriteLine("Введите коордигату Х точки B");
-    coordXPointB = int.Parse(Console.ReadLine());
+    Console.WriteLine("Введите координату Х точки B");
+    coordXPointB = int.Parse(Console.ReadLine()?? "");
 
-    Console.WriteLine("Введите коордигату Y точки B");
-    coordYPointB = int.Parse(Console.ReadLine());
+    Console.WriteLine("Введите координату Y точки B");
+    coordYPointB = int.Parse(Console.ReadLine()?? "");
 
-    Console.WriteLine("Введите коордигату Z точки B");
-    coordZPointB = int.Parse(Console.ReadLine());
+    Console.WriteLine("Введите координату Z точки B");
+    coordZPointB = int.Parse(Console.ReadLine()?? "");
 }
 
 // Вычисляет расстояние между точками А и В
