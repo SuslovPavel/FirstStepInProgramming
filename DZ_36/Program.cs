6e88385bf2e169c08507ca87e873dd320feca6e3
SuslovPavel <pavel7511084@gmail.com>
2022-08-21 00:55:10 +0500

    Added Comment

---
+++ DZ_36/Program.cs
@@ -0,0 +1,63 @@
+﻿//  Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+//  Пример: [3, 7, 23, 12] -> 19
+//          [3, 7, -2, 1] -> 8
+//          [-4, -6, 89, 6] -> 0
+
+int[] FillingArrey()  //  Метод возвращает массив заполняя случайными числами
+{
+    int[] outArray = new int[8];                            // создаём переменную, выделяем память под неё, задаём длину массива 8 элементов
+    int i = 0;                                               // индекс массива
+    System.Random numberSintezator = new System.Random();    // создаём синтезатор случайных чисел, выделяем под него память
+    while (i < 8)                                           // Цикл заполняет массив
+    {
+        outArray[i] = numberSintezator.Next(-99, 100);       // Запускает синтезатор в границах от -99 до 100
+        i++;
+    }
+    return outArray;
+}
+
+void PrintIntArray(int[] inputArray)  //  Метод печатает массив
+{
+    int i = 0;
+    while (i < inputArray.Length - 1)                       //Пробегаем все элементы массива       
+    {
+        Console.Write(inputArray[i] + ", ");                 //Выводим элемент массива
+        i++;
+    }
+    Console.WriteLine(inputArray[i]);                        //Выводим элемент массива, строка необходима чтобы устранить запятую
+}
+
+int CalculateTask(int[] outArrey)  // принимает массив который заполнен в предыдущем методе "int [] FillingArrey()"
+{
+    int i = 0;
+    int result = 0;                                    //  переменная для подсчёта элементов на чётных позициях массива
+    while (i < outArrey.Length)                        //  ограничивает цикл длиною массива
+    {
+        if ((i) % 2 != 0)                                // ищет чётные позиции массива
+        {
+            result += outArrey[i];                       // сумирует элементы стоящие на чётных позициях массива  
+        }
+        i++;
+    }
+    return result;
+}
+
+int MethodSimple(int[] outArrey)  // принимает массив который заполнен в предыдущем методе "int [] FillingArrey()"
+{
+    int i = 1;
+    int sum = 0;                                    //  переменная для подсчёта элементов на чётных позициях массива
+    while (i < outArrey.Length)                     //  ограничивает цикл длиною массива
+    {
+        sum = sum + outArrey[i];                    //  сумирует элементы на чётных позициях
+        i+=2;                                       //  икримент (+2) позволяющий пропустить элементы массива не чётных позиций 
+    }
+    return sum;
+}
+    
+
+
+int[] bufferArray = FillingArrey();                      // заполняет массив
+PrintIntArray(bufferArray);                              // печатает массив        
+Console.Write("сумма значений на чётных позициях массива=  " + CalculateTask(bufferArray));  // Запускает метод CalculateTask, закладывая переменную в bufferArray
+Console.WriteLine();
+Console.Write("сумма значений на чётных позициях массива=  " + MethodSimple(bufferArray));   // Запускает метод MethodSimple, закладывая переменную в bufferArray
---
+++ DZ_38/Program.cs
@@ -0,0 +1,53 @@
+﻿//  Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+//  Пример: [3 7 22 2 78] -> 76
+
+
+
+int[] FillingArrey()  //  Метод возвращает массив заполняя случайными числами
+{
+    int[] outArray = new int[8];                            // создаём переменную, выделяем память под неё, задаём длину массива 8 элементов
+    int i = 0;                                               // индекс массива
+    System.Random numberSintezator = new System.Random();    // создаём синтезатор случайных чисел, выделяем под него память
+    while (i < 8)                                           // Цикл заполняет массив
+    {
+        outArray[i] = numberSintezator.Next(0,100);       // Запускает синтезатор в границах от 0 до 100
+        i++;
+    }
+    return outArray;
+}
+
+void PrintIntArray(int[] inputArray)  //  Метод печатает массив
+{
+    int i = 0;
+    while (i < inputArray.Length - 1)                       //Пробегаем все элементы массива       
+    {
+        Console.Write(inputArray[i] + ", ");                 //Выводим элемент массива
+        i++;
+    }
+    Console.WriteLine(inputArray[i]);                        //Выводим элемент массива, строка необходима чтобы устранить запятую
+}
+
+int CalculateTask(int[] outArrey)  // принимает массив который заполнен в предыдущем методе "int [] FillingArrey()"
+{
+    int i = 0;
+    int max = 0;                                       //  переменная для максимального значения
+    int min = int.MaxValue;                            //  переменная для минимального значения ( команда int.MaxValue ищет максимальное значение)
+    int difference = 0;                                //  переменная для подсчёта разници между max и min
+    while (i < outArrey.Length)                        //  ограничивает цикл длиною массива
+    {
+        if (outArrey[i] > max)                         //  ищет максимальное значение массива
+        {
+            max=outArrey[i];                           //  присваивает максимальное значение элементу массива  
+        }    
+        if (outArrey[i] < min)                         //  ищет минимальное значение массива
+        {
+            min=outArrey[i];                           //  присваивает минимальное значение массиву
+        }
+        i++;
+    }
+    return difference = (max-min);                     //  вычисляет разницу между минимальным и максимальным значением массива
+}
+
+int[] bufferArray = FillingArrey();                      // заполняет массив
+PrintIntArray(bufferArray);                              // печатает массив        
+Console.Write("разница между max и min значением массива=  " + CalculateTask(bufferArray));  // Запускает метод CalculateTask, закладывая переменную difference в bufferArray
